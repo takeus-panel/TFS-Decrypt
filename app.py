a3589293f54ea4562d98fe005ea72d1cb0ba51af
Takeus <68923554+pomme2terree@users.noreply.github.com>
2021-11-27 13:28:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,13 @@ from file manager
 Takeus 2021
 ------
 """)
+time.sleep(1)
 
 file_path = input('File: ')
 file_final = os.path.splitext(file_path)[0]
+
+time.sleep(1)
+print('\nplease wait')
 
 # opening the encrypted file
 with open('{}'.format(file_path), 'rb') as enc_file:
